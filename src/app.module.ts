7a7de484142ec7c3ce8d6b398b04f33bbc32edde
Arnaud Igor N'takpe <arnaud.ntakpe@mpayment.africa>
2026-03-24 12:55:16 +0000

    feat: add cron job for contact render api MRA-6

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,18 @@ import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { DatabaseModule } from './database/database.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CronJobService } from './services/cron-job/cron-job.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
     HttpModule,
     DatabaseModule,
     FavoritesModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CronJobService],
 })
 export class AppModule {}
